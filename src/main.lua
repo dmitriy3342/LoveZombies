6ce73995db3d221cf6aa87c3293ab0d239e36d50
Angald Dmitriy <dmitriy3342@mail.ru>
2014-04-13 14:58:02 +0700

    Рисую линии

---
+++ src/main.lua
@@ -0,0 +1,25 @@
+function love.load()
+    width=900
+    height=600
+    
+    love.window.setMode(w,h)--размер экрана 
+end
+
+
+function love.draw()--Функция рисует
+    
+    x 
+    
+    for i=1,w do
+        love.graphics.line()
+         --love.graphics.setColor(balls[i].color.r,balls[i].color.g,balls[i].color.b,255)
+         --love.graphics.circle("fill",balls[i].x,balls[i].y,balls[i].r,100)
+    end
+
+end
+
+function love.update(dt)
+
+  
+end
+  
